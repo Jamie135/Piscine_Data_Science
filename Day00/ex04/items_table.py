b163e008c1e3217011affe2702d5f3fb10084ef9
Jooma <jamie.gworld@hotmail.com>
2024-08-18 14:22:30 +0200

    ex04 done

---
+++ Day00/ex04/items_table.py
@@ -0,0 +1,31 @@
+import psycopg2
+from psycopg2 import OperationalError
+
+
+def main():
+    """create a table"""
+    try:
+        connection = psycopg2.connect(
+            database="piscineds",
+            user="pbureera",
+            password="mysecretpassword",
+            host="localhost",
+            port="5432",
+        )
+        print("Connected to PostgreSQL Database")
+        with open("items_table.sql", "r") as sql:
+            query = sql.read()
+        connection.autocommit = True
+        cursor = connection.cursor()
+        cursor.execute(query)
+        print("Query executed successfully")
+        cursor.close()
+        connection.close()
+    except OperationalError as o:
+        print(f"OperationalError: {o}")
+    except Exception as e:
+        print(f"AssertionError: {e}")
+
+
+if __name__ == "__main__":
+    main()
