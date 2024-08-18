9eeaa341cac8e48c3d62be8a9132ecde5e80c345
Jooma <jamie.gworld@hotmail.com>
2024-08-18 21:57:30 +0200

    ex03

--- Day01/ex03/fusion.sql
+++ Day01/ex03/fusion.sql
@@ -3,10 +3,12 @@ ADD COLUMN category_id BIGINT,
 ADD COLUMN category_code VARCHAR(255),
 ADD COLUMN brand VARCHAR(255);
 
-UPDATE customers c
-LEFT JOIN items i
-ON c.product_id = i.product_id
+UPDATE customers
 SET 
-    c.category_id = i.category_id,
-    c.category_code = i.category_code,
-    c.brand = i.brand;
+    category_id = i.category_id,
+    category_code = i.category_code,
+    brand = i.brand
+FROM 
+    items i
+WHERE 
+    customers.product_id = i.product_id;
