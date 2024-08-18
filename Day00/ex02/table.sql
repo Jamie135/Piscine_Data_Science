38a81a33b5caa3664d7a69be02bc285c9d331a60
Jooma <jamie.gworld@hotmail.com>
2024-08-18 21:33:11 +0200

    ex02 done

--- Day00/ex02/table.sql
+++ Day00/ex02/table.sql
@@ -2,9 +2,6 @@ CREATE TABLE IF NOT EXISTS data_2022_oct (
     event_time TIMESTAMP,
     event_type VARCHAR(255),
     product_id INT,
-    -- category_id BIGINT,
-    -- category_code VARCHAR,
-    -- brand VARCHAR,
     price FLOAT,
     user_id BIGINT,
     user_session UUID
