0ba1eb89b1d4ee6c2b1f3ac7ab59bc62aedbdcac
Jamie135 <pbureera@paul-f4Br2s2.clusters.42paris.fr>
2024-08-19 13:42:58 +0200

    updated copy.sh

--- copy.sh
+++ copy.sh
@@ -1,13 +1,13 @@
-# docker cp /Users/pbureera/goinfre/subject/customer/data_2022_dec.csv postgres:/tmp/data_2022_dec.csv
-# docker cp /Users/pbureera/goinfre/subject/customer/data_2022_nov.csv postgres:/tmp/data_2022_nov.csv
-# docker cp /Users/pbureera/goinfre/subject/customer/data_2022_oct.csv postgres:/tmp/data_2022_oct.csv
-# docker cp /Users/pbureera/goinfre/subject/customer/data_2023_jan.csv postgres:/tmp/data_2023_jan.csv
-# docker cp /Users/pbureera/goinfre/subject/customer/data_2023_feb.csv postgres:/tmp/data_2023_feb.csv
-# docker cp /Users/pbureera/goinfre/subject/item/item.csv postgres:/tmp/item.csv
+docker cp /mnt/nfs/homes/pbureera/goinfre/subject/customer/data_2022_dec.csv postgres:/tmp/data_2022_dec.csv
+docker cp /mnt/nfs/homes/pbureera/goinfre/subject/customer/data_2022_nov.csv postgres:/tmp/data_2022_nov.csv
+docker cp /mnt/nfs/homes/pbureera/goinfre/subject/customer/data_2022_oct.csv postgres:/tmp/data_2022_oct.csv
+docker cp /mnt/nfs/homes/pbureera/goinfre/subject/customer/data_2023_jan.csv postgres:/tmp/data_2023_jan.csv
+docker cp /mnt/nfs/homes/pbureera/goinfre/subject/customer/data_2023_feb.csv postgres:/tmp/data_2023_feb.csv
+docker cp /mnt/nfs/homes/pbureera/goinfre/subject/item/item.csv postgres:/tmp/item.csv
 
-docker cp ~/Documents/42/Data/subject/customer/data_2022_dec.csv postgres:/tmp/data_2022_dec.csv
-docker cp ~/Documents/42/Data/subject/customer/data_2022_nov.csv postgres:/tmp/data_2022_nov.csv
-docker cp ~/Documents/42/Data/subject/customer/data_2022_oct.csv postgres:/tmp/data_2022_oct.csv
-docker cp ~/Documents/42/Data/subject/customer/data_2023_jan.csv postgres:/tmp/data_2023_jan.csv
-docker cp ~/Documents/42/Data/subject/customer/data_2023_feb.csv postgres:/tmp/data_2023_feb.csv
-docker cp ~/Documents/42/Data/subject/item/item.csv postgres:/tmp/item.csv
+# docker cp ~/Documents/42/Data/subject/customer/data_2022_dec.csv postgres:/tmp/data_2022_dec.csv
+# docker cp ~/Documents/42/Data/subject/customer/data_2022_nov.csv postgres:/tmp/data_2022_nov.csv
+# docker cp ~/Documents/42/Data/subject/customer/data_2022_oct.csv postgres:/tmp/data_2022_oct.csv
+# docker cp ~/Documents/42/Data/subject/customer/data_2023_jan.csv postgres:/tmp/data_2023_jan.csv
+# docker cp ~/Documents/42/Data/subject/customer/data_2023_feb.csv postgres:/tmp/data_2023_feb.csv
+# docker cp ~/Documents/42/Data/subject/item/item.csv postgres:/tmp/item.csv
