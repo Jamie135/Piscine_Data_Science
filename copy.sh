3396db0e17e5872d04146aae5133a9add9d53289
Jamie135 <pbureera@made-f0Cr6s1.clusters.42paris.fr>
2024-08-20 11:05:15 +0200

    updated copy.sh

--- copy.sh
+++ copy.sh
@@ -1,8 +1,8 @@
-docker cp /mnt/nfs/homes/pbureera/goinfre/subject/customer/data_2022_dec.csv postgres:/tmp/data_2022_dec.csv
-docker cp /mnt/nfs/homes/pbureera/goinfre/subject/customer/data_2022_nov.csv postgres:/tmp/data_2022_nov.csv
-docker cp /mnt/nfs/homes/pbureera/goinfre/subject/customer/data_2022_oct.csv postgres:/tmp/data_2022_oct.csv
-docker cp /mnt/nfs/homes/pbureera/goinfre/subject/customer/data_2023_jan.csv postgres:/tmp/data_2023_jan.csv
-docker cp /mnt/nfs/homes/pbureera/goinfre/subject/item/item.csv postgres:/tmp/item.csv
+docker cp /mnt/nfs/homes/pbureera/sgoinfre/subject/customer/data_2022_dec.csv postgres:/tmp/data_2022_dec.csv
+docker cp /mnt/nfs/homes/pbureera/sgoinfre/subject/customer/data_2022_nov.csv postgres:/tmp/data_2022_nov.csv
+docker cp /mnt/nfs/homes/pbureera/sgoinfre/subject/customer/data_2022_oct.csv postgres:/tmp/data_2022_oct.csv
+docker cp /mnt/nfs/homes/pbureera/sgoinfre/subject/customer/data_2023_jan.csv postgres:/tmp/data_2023_jan.csv
+docker cp /mnt/nfs/homes/pbureera/sgoinfre/subject/item/item.csv postgres:/tmp/item.csv
 
 # docker cp ~/Documents/42/Data/subject/customer/data_2022_dec.csv postgres:/tmp/data_2022_dec.csv
 # docker cp ~/Documents/42/Data/subject/customer/data_2022_nov.csv postgres:/tmp/data_2022_nov.csv
