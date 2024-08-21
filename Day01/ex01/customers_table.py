a1584c4e9f1ad2167d2942f5be5362baf31c75dd
Jamie135 <pbureera@made-f0Br3s10.clusters.42paris.fr>
2024-08-21 14:07:20 +0200

    ex01 done

--- Day01/ex01/customers_table.py
+++ Day01/ex01/customers_table.py
@@ -1,9 +1,11 @@
+import os
 import psycopg2
-from psycopg2 import OperationalError
+import subprocess
+import pandas as pd
 
 
 def main():
-    """create a table"""
+    """create customers table"""
     try:
         connection = psycopg2.connect(
             database="piscineds",
@@ -13,19 +15,41 @@ def main():
             port="5432",
         )
         print("Connected to PostgreSQL Database")
-        with open("customers_table.sql", "r") as sql:
-            query = sql.read()
+        # path = os.path.expanduser("~/Documents/42/Data/subject/customer")
+        path = "/mnt/nfs/homes/pbureera/sgoinfre/eval/customer/"
         connection.autocommit = True
         cursor = connection.cursor()
-        cursor.execute(query)
+        for file in os.listdir(path):
+            file_path = os.path.join(path, file)
+            data = pd.read_csv(file_path, nrows=1)
+            columns_lst = data.columns.to_list()
+            columns = (" ,").join(columns_lst)
+            subprocess.run(
+                ["docker", "cp", file_path, f"postgres:/tmp/{file}"]
+                )
+            create = f"""CREATE TABLE IF NOT EXISTS customers (
+                event_time TIMESTAMP,
+                event_type VARCHAR(255),
+                product_id INT,
+                price FLOAT,
+                user_id BIGINT,
+                user_session UUID
+                );
+                COPY customers({columns}) FROM '/tmp/{file}' CSV HEADER;
+                """
+            cursor.execute(create)
+            subprocess.run(
+                ["docker", "exec", "postgres", "rm", f"/tmp/{file}"]
+                )
         print("Query executed successfully")
         cursor.close()
         connection.close()
-    except OperationalError as o:
-        print(f"OperationalError: {o}")
     except Exception as e:
         print(f"Error: {e}")
 
 
 if __name__ == "__main__":
     main()
+
+
+# SELECT * FROM customers WHERE EXTRACT(YEAR FROM event_time) > 2022;
