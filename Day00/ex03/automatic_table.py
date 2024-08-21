b876dc341dc5cdc4d061436ccf9041ba7f1fcb7a
Jooma <jamie.gworld@hotmail.com>
2024-08-21 11:48:07 +0200

    Day00 done

--- Day00/ex03/automatic_table.py
+++ Day00/ex03/automatic_table.py
@@ -1,11 +1,12 @@
 import os
 import psycopg2
-from psycopg2 import OperationalError
+import subprocess
 
 
 def main():
     """create a table for all data_202*_***.csv files"""
     try:
+
         connection = psycopg2.connect(
             database="piscineds",
             user="pbureera",
@@ -14,10 +15,13 @@ def main():
             port="5432",
         )
         print("Connected to PostgreSQL Database")
-        csvpath = "/mnt/nfs/homes/pbureera/sgoinfre/eval/customer/"
+        path = os.path.expanduser("~/Documents/42/Data/subject/customer")
+        # path = "/mnt/nfs/homes/pbureera/sgoinfre/eval/customer/"
         connection.autocommit = True
         cursor = connection.cursor()
-        for file in os.listdir(csvpath):
+        for file in os.listdir(path):
+            file_path = os.path.join(path, file)
+            subprocess.run(["docker", "cp", file_path, f"postgres:/tmp/{file}"])
             table = os.path.splitext(file)[0]
             create = f"""CREATE TABLE IF NOT EXISTS {table} (
                 event_time TIMESTAMP,
@@ -30,11 +34,10 @@ def main():
                 COPY {table} FROM '/tmp/{table}.csv' CSV HEADER;
                 """
             cursor.execute(create)
+            subprocess.run(["docker", "exec", "postgres", "rm", f"/tmp/{file}"])
         print("Query executed successfully")
         cursor.close()
         connection.close()
-    except OperationalError as o:
-        print(f"OperationalError: {o}")
     except Exception as e:
         print(f"Error: {e}")
 
