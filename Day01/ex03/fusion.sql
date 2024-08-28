f3413841063e37d05e9aba4aa6ee5f9e57c86e3b
Jamie135 <pbureera@made-f0Cr8s2.clusters.42paris.fr>
2024-08-28 15:21:55 +0200

    ex03 done

--- Day01/ex03/fusion.sql
+++ Day01/ex03/fusion.sql
@@ -1,32 +1,40 @@
-CREATE TABLE tmp AS
-SELECT c.*, category_id, category_code, brand
+CREATE TABLE tmp_customers (
+    event_time TIMESTAMP NOT NULL,
+    event_type VARCHAR(255),
+    product_id INT,
+    price FLOAT,
+    user_id BIGINT,
+    user_session UUID,
+    category_id BIGINT,
+    category_code VARCHAR(255),
+    brand VARCHAR(255));
+
+CREATE TABLE tmp_items(
+    product_id INT,
+    category_id BIGINT,
+    category_code VARCHAR(255),
+    brand VARCHAR(255));
+
+INSERT INTO tmp_items (product_id, category_id,
+category_code, brand)
+SELECT
+    "product_id",
+    MAX("category_id") AS "category_id",
+    MAX("category_code") AS "category_code",
+    MAX("brand") AS "brand"
+FROM items
+GROUP BY "product_id";
+
+INSERT INTO tmp_customers
+SELECT c.event_time, c.event_type, c.product_id,
+c.price, c.user_id, c.user_session,
+    i.category_id, i.category_code, i.brand
 FROM customers c
-LEFT JOIN items i ON c.product_id = i.product_id;
+LEFT JOIN tmp_items i ON c.product_id = i.product_id
+ORDER BY c.event_time;
 
-DROP TABLE IF EXISTS customers;
-ALTER TABLE tmp RENAME TO customers
+DROP TABLE customers;
 
--- CREATE TABLE tmp AS (
---     SELECT 
---         c.event_time, 
---         c.event_type,
---         c.product_id,
---         category_id,
---         category_code,
---         brand,
---         c.price,
---         c.user_id,
---         c.user_session
---     FROM 
---         customers c
---     JOIN 
---         items i 
---     ON 
---         c.product_id = i.product_id
---     ORDER BY 
---         c.event_time
--- );
+DROP TABLE tmp_items;
 
--- DROP TABLE customers;
-
--- ALTER TABLE tmp RENAME TO customers;
+ALTER TABLE tmp_customers RENAME TO customers;
