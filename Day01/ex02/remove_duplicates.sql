1250ff3f5af8ea0823249d53682641d1df9ae00d
Jamie135 <pbureera@made-f0Cr8s2.clusters.42paris.fr>
2024-08-28 14:08:09 +0200

    ex02 done

--- Day01/ex02/remove_duplicates.sql
+++ Day01/ex02/remove_duplicates.sql
@@ -1,31 +1,33 @@
-WITH cte AS (
-    SELECT *,
-        LAG(event_time) OVER (PARTITION BY event_type, product_id, price, user_id, user_session ORDER BY event_time) AS lag
-    FROM customers
-)
-DELETE FROM customers
-USING cte
-WHERE customers.event_time = cte.event_time
-AND customers.event_type = cte.event_type
-AND customers.product_id = cte.product_id
-AND customers.price = cte.price
-AND customers.user_id = cte.user_id
-AND customers.user_session = cte.user_session
-AND (cte.event_time - cte.lag <= INTERVAL '1 second' AND cte.lag IS NOT NULL);
+CREATE TEMPORARY TABLE tmp AS 
+SELECT * FROM customers;
 
+-- from the temporary table, remove all duplicates rows.
+-- we use ctid to have the unique identifier of the row
+-- PARTITION : fonction ROW_NUMBER() attribue num ligne pour chaque groupe
+-- (SELECT ctid, ROW_NUMBER() OVER(..) AS row_num FROM cust) AS duplicates:
+-- fonction ROW_NUMBER() attribue num ligne à chaque groupe en double,
+-- où groupe défini par colonnes spécifiées dans la clause PARTITION BY.
+DELETE FROM tmp WHERE ctid IN (
+SELECT ctid FROM (
+    SELECT ctid, ROW_NUMBER() OVER(
+            PARTITION BY date_trunc('minute', event_time),
+            event_type,
+            product_id,
+            price,
+            user_id,
+            user_session ORDER BY event_time DESC
+        ) AS row_num FROM customers
+    ) AS duplicates WHERE duplicates.row_num > 1 );
 
--- CREATE TABLE tmp AS
--- WITH cte AS (
---     SELECT *,
---     LAG(event_time) OVER (PARTITION BY event_type, product_id, price, user_id, user_session ORDER BY event_time) AS lag
---     FROM customers
--- )
--- SELECT event_time, event_type, product_id, price, user_id, user_session
--- FROM cte
--- WHERE event_time - lag > INTERVAL '1 second'
--- OR lag IS NULL;
--- DROP TABLE customers;
--- ALTER TABLE tmp RENAME TO customers;
+DROP TABLE customers;
+
+CREATE TABLE customers AS
+SELECT *
+FROM tmp
+ORDER BY event_time;
+
+DROP TABLE tmp;
+
 
 
 -- SELECT COUNT(*) AS total_rows FROM customers;
