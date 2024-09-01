6192a74a8098daf5e708035e27f874a7e5957be0
Jooma <jamie.gworld@hotmail.com>
2024-09-01 17:52:10 +0200

    updated copy.sh

--- copy.sh
+++ copy.sh
@@ -5,13 +5,6 @@ docker cp /mnt/nfs/homes/pbureera/sgoinfre/subject/customer/data_2023_jan.csv po
 docker cp /mnt/nfs/homes/pbureera/sgoinfre/subject/customer/data_2023_feb.csv postgres:/tmp/data_2023_feb.csv
 docker cp /mnt/nfs/homes/pbureera/sgoinfre/subject/item/item.csv postgres:/tmp/item.csv
 
-# docker cp /mnt/nfs/homes/pbureera/sgoinfre/eval/customer/data_2022_dec.csv postgres:/tmp/data_2022_dec.csv
-# docker cp /mnt/nfs/homes/pbureera/sgoinfre/eval/customer/data_2022_nov.csv postgres:/tmp/data_2022_nov.csv
-# docker cp /mnt/nfs/homes/pbureera/sgoinfre/eval/customer/data_2022_oct.csv postgres:/tmp/data_2022_oct.csv
-# docker cp /mnt/nfs/homes/pbureera/sgoinfre/eval/customer/data_2023_jan.csv postgres:/tmp/data_2023_jan.csv
-# docker cp /mnt/nfs/homes/pbureera/sgoinfre/eval/customer/data_2023_feb.csv postgres:/tmp/data_2023_feb.csv
-# docker cp /mnt/nfs/homes/pbureera/sgoinfre/eval/item/item.csv postgres:/tmp/item.csv
-
 # docker cp ~/Documents/42/Data/subject/customer/data_2022_dec.csv postgres:/tmp/data_2022_dec.csv
 # docker cp ~/Documents/42/Data/subject/customer/data_2022_nov.csv postgres:/tmp/data_2022_nov.csv
 # docker cp ~/Documents/42/Data/subject/customer/data_2022_oct.csv postgres:/tmp/data_2022_oct.csv
