cd6e83f9bc494674a54c7f271705408d20f5a1c7
Jooma <jamie.gworld@hotmail.com>
2024-09-11 22:18:51 +0200

    es06

--- Day04/ex06/democracy.py
+++ Day04/ex06/democracy.py
@@ -30,86 +30,38 @@ def main():
     if len(sys.argv) != 3 or "Train_knight.csv" not in sys.argv[1] or "Test_knight.csv" not in sys.argv[2]:
         print("Usage: python3 Tree.py /path/to/Train_knight.csv /path/to/Test_knight.csv")
         sys.exit(1)
-    train_knight = load(sys.argv[1])
-    test_knight = load(sys.argv[2])
-    if train_knight is not None and test_knight is not None:
-        x = train_knight.drop(
-            columns=[
-                "knight",
-                "Prescience",
-                "Push",
-                "Deflection",
-                "Survival",
-                "Midi-chlorien",
-                "Grasping",
-                "Pull",
-                "Awareness",
-                "Repulse",
-                "Attunement",
-                "Empowered",
-                "Dexterity",
-                "Delay",
-                "Slash",
-                "Sprint",
-                "Sensitivity",
-                "Stims",
-                "Strength",
-                "Recovery",
-                "Hability",
-            ]
-        )
-        scaler = StandardScaler()
-        x = pd.DataFrame(scaler.fit_transform(x), columns=x.columns)
-        y = train_knight["knight"]
-        x_train, x_val, y_train, y_val = train_test_split(
-            x, y, test_size=0.2, random_state=42
-        )
+    df_train = load(sys.argv[1])
+    df_test = load(sys.argv[2])
 
-        model1 = DecisionTreeClassifier(random_state=13)
-        model2 = KNeighborsClassifier(n_neighbors=9)
-        model3 = LogisticRegression(max_iter=1000, random_state=123)
+    scaler = StandardScaler()
+    x = df_train.drop(columns=["knight"])
+    x = pd.DataFrame(scaler.fit_transform(x), columns=x.columns)
+    y = df_train["knight"]
+    x_train, x_valid, y_train, y_valid = train_test_split(
+        x, y, test_size=0.2, random_state=42
+    )
 
-        voting = VotingClassifier(
-            estimators=[("dt", model1), ("knn", model2), ("lg", model3)],
-            voting="hard",
-        )
-        voting.fit(x_train, y_train)
-        predicted_val = voting.predict(x_val)
-        accuracy = accuracy_score(y_val, predicted_val)
-        print(f"Accuracy: {accuracy}")
-        print(
-            f"F1_score: {round(f1_score(y_val, predicted_val, average='macro'), 4)}"
-        )
+    model1 = DecisionTreeClassifier(random_state=42)
+    model2 = KNeighborsClassifier(n_neighbors=10)
+    model3 = LogisticRegression(max_iter=1000, random_state=42)
 
-        x = test_knight.drop(
-            columns=[
-                "Prescience",
-                "Push",
-                "Deflection",
-                "Survival",
-                "Midi-chlorien",
-                "Grasping",
-                "Pull",
-                "Awareness",
-                "Repulse",
-                "Attunement",
-                "Empowered",
-                "Dexterity",
-                "Delay",
-                "Slash",
-                "Sprint",
-                "Sensitivity",
-                "Stims",
-                "Strength",
-                "Recovery",
-                "Hability",
-            ]
-        )
-        test_knight = pd.DataFrame(scaler.fit_transform(x), columns=x.columns)
-        predicted_test = voting.predict(test_knight)
-        with open("Voting.txt", "w") as output:
-            for item in predicted_test:
-                output.write(item + "\n")
+    voting = VotingClassifier(
+        estimators=[("dt", model1), ("knn", model2), ("lg", model3)],
+        voting="hard",
+    )
+    voting.fit(x_train, y_train)
+    predicted_val = voting.predict(x_valid)
+    accuracy = accuracy_score(y_valid, predicted_val)
+    print(f"Accuracy: {accuracy}")
+    print(
+        f"F1_score: {round(f1_score(y_valid, predicted_val, average='macro'), 4)}"
+    )
+
+    df_test = pd.DataFrame(scaler.fit_transform(df_test), columns=df_test.columns)
+    predicted_test = voting.predict(df_test)
+    with open("Voting.txt", "w") as output:
+        for item in predicted_test:
+            output.write(item + "\n")
 
 
 if __name__ == "__main__":
